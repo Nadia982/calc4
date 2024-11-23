f5578b19ea2337a18a48a62eefda9ec0f6ce283d
Natalie Gillam <ngillam@hotmail.co.uk>
2024-11-23 01:20:40 +0000

    calculate result if 'expressionToCalculate' length is 3 and an operator key is pressed

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 // to do ********************************
-// backspace
-// decimal
 // deal with long numbers
 // roman
 // words
-// calculate result if an operator key is pressed
-// if I only put one number and press equals, show that one number in result.
 
 console.clear();
 const numberButtons = document.querySelectorAll(".num");
@@ -23,17 +19,17 @@ const equals = document.getElementById("equals");
 let operators = ["+", "-", "*", "/"];
 let answer;
 
-function reset(prevAnswer) {
+function prepareForFurtherCalculations(prevAnswer) {
   display.innerText = prevAnswer;
   expressionToCalculate.length = 0;
   leftOperand = prevAnswer;
-  rightOperand = 0;
+  rightOperand = "";
   expressionToCalculate[0] = leftOperand;
 }
 
 function processAnswer(answer) {
   result.innerText = answer;
-  reset(answer);
+  prepareForFurtherCalculations(answer);
 }
 
 function processExpressionToCalculate(expressionArray) {
@@ -69,19 +65,18 @@ function processInput(input) {
       expressionToCalculate.length === 1) &&
     numbers.includes(input)
   ) {
-    if (display.innerHTML === '0') {
+    if (display.innerText === "0") {
       display.innerText = input;
       leftOperand = input;
       expressionToCalculate[0] = input;
     } else {
-      if (input == "." && !leftOperand.includes(".")) {
+      if (input === "." && !leftOperand.includes(".")) {
         display.innerText += input;
         leftOperand += input;
-      } else {
+      } else if (input !== ".") {
         display.innerText += input;
         leftOperand += input;
       }
-
       expressionToCalculate[0] = leftOperand;
     }
   }
@@ -90,35 +85,48 @@ function processInput(input) {
     operator = input;
     expressionToCalculate.push(operator);
     display.innerText += input;
-    
   }
   // Processing rightOperand
-  if (expressionToCalculate.length == 2 && numbers.includes(input)) {
-    if (rightOperand === "0") {
+  if (
+    (expressionToCalculate.length == 2 || expressionToCalculate.length == 3) &&
+    numbers.includes(input)
+  ) {
+    if (rightOperand === "0" && input !== ".") {
       display.innerText = input;
       rightOperand = input;
-      expressionToCalculate[2] = input;
-    } else if (input == "." && !rightOperand.includes(".")) {
-      display.innerText += input;
-      rightOperand += input;
     } else {
-      display.innerText += input;
-      rightOperand += input;
+      if (input === "." && !rightOperand.includes(".")) {
+        display.innerText += input;
+        rightOperand += input;
+        console.log(rightOperand);
+      } else if (input !== ".") {
+        display.innerText += input;
+        rightOperand += input;
+        console.log(rightOperand);
+      }
     }
     expressionToCalculate[2] = rightOperand;
     console.log("expression to calculate", expressionToCalculate);
-    // adds eventListener to equals button
-    
   }
-  console.log("expression to calculate", expressionToCalculate);
 }
 
 equals.addEventListener("click", () => {
-  if (expressionToCalculate.length == 2) {
-    expressionToCalculate.push(rightOperand);
-    console.log("expression to calculate:", expressionToCalculate);
+  if (expressionToCalculate.length === 1 && expressionToCalculate[0] === "") {
+    result.innerText = 0;
+  } else if (expressionToCalculate.length === 1) {
+    result.innerText = expressionToCalculate[0];
+  } else if (expressionToCalculate.length == 3) {
     processExpressionToCalculate(expressionToCalculate);
   }
+});
+
+operatorButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    console.log("hello");
+    if (expressionToCalculate.length == 3) {
+      processExpressionToCalculate(expressionToCalculate);
+    }
+  });
 });
 
 function clearDisplay() {
@@ -126,27 +134,24 @@ function clearDisplay() {
   display.innerText = "";
   result.innerText = 0;
   expressionToCalculate.length = 0;
-  leftOperand = 0;
-  rightOperand = 0;
+  leftOperand = "";
+  rightOperand = "";
 }
 
 numbersAndOperators.forEach((btn) => {
   btn.addEventListener("click", () => {
     const value = btn.dataset.value;
     processInput(value);
-    // const currentInput = document.querySelector("#input").value;
-    // document.querySelector("#input").value = currentInput + button.textContent;
   });
 });
 
-
-// function backspace() {
-//   console.log("expressionToCalculate: ", expressionToCalculate)
-//   display.innerText = display.innerText.slice(0, -1);
-//   expressionAsString = expressionToCalculate.join(",");
-//   console.log(expressionAsString);
-//   expressionAsString = expressionAsString.slice(0, -1)
-//   console.log(expressionAsString);
-//   expressionToCalculate =expressionAsString.split(",");
-//   console.log(expressionToCalculate);
-// }
+function backspace() {
+  console.log("expressionToCalculate: ", expressionToCalculate);
+  display.innerText = display.innerText.slice(0, -1);
+  expressionAsString = expressionToCalculate.join(",");
+  console.log(expressionAsString);
+  expressionAsString = expressionAsString.slice(0, -1);
+  console.log(expressionAsString);
+  expressionToCalculate = expressionAsString.split(",");
+  console.log(expressionToCalculate);
+}
